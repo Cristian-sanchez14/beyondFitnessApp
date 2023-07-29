62f17b4cffbb6bb36293c4f9ea76c5e101b08f67
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-07-29 15:03:51 -0600

    video Post added

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,14 +1,33 @@
-import {View, FlatList} from 'react-native';
+import {View, FlatList, ViewabilityConfig, ViewToken} from 'react-native';
 import FeedPost from '../../components/FeedPost';
 import posts from '../../assets/data/posts.json';
+import {useRef, useState} from 'react';
 
 const HomeScreen = () => {
+  const [activePostId, setActivePostId] = useState<null | string>(null);
+
+  const viewabilityConfig: ViewabilityConfig = {
+    itemVisiblePercentThreshold: 51,
+  };
+
+  const onViewableItemsChanged = useRef(
+    ({viewableItems}: {viewableItems: Array<ViewToken>}) => {
+      if (viewableItems.length > 0) {
+        setActivePostId(viewableItems[0].item.id);
+      }
+    },
+  );
+
   return (
     <View>
       <FlatList
         data={posts}
-        renderItem={({item}) => <FeedPost post={item} />}
+        renderItem={({item}) => (
+          <FeedPost isVisible={item.id === activePostId} post={item} />
+        )}
         showsVerticalScrollIndicator={false}
+        viewabilityConfig={viewabilityConfig}
+        onViewableItemsChanged={onViewableItemsChanged.current}
       />
     </View>
   );
