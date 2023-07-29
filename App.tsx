18bffc454aac32d50710411cc8c459834baa0a22
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-07-29 15:56:51 -0600

    updating edit screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 import {View, StyleSheet} from 'react-native';
 // import HomeScreen from './src/screens/HomeScreen/HomeScreen';
-<<<<<<< HEAD
-=======
-// import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
-// import ProfileScreen from './src/screens/ProfileScreen';
->>>>>>> UI
 import EditProfileScreen from './src/screens/EditProfileScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
-<<<<<<< HEAD
-      {/* <HomeScreen /> */}
-=======
->>>>>>> UI
       <EditProfileScreen />
     </View>
   );
